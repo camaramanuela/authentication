c441dc3899a851147de52b1b8e1f2f969f620b9d
manuelabognar <manuela.camara@usp.br>
2020-04-29 09:37:50 -0300

    logout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+import AuthContext from '../../contexts/auth'
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent: 'center'
+  }
+});
 
 const Dashboard: React.FC = () => {
+  const { signed, user, signOut } = useContext(AuthContext); 
+
+  console.log (signed);
+  console.log (user);
+
+  function handleSignOut () {
+    signOut();
+  }
+
   return (
-    <View>
-      <Text>Dashboard ok =]</Text>
+    <View style={styles.container} >
+      <Button title="Logout" onPress={handleSignOut} />
     </View>
-  );
-}
+  )
+
+};
 
 export default Dashboard;
